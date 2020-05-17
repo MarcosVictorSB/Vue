efcac12cb384be40d64e429c0bae17939d052360
Marcos Victor <marcos@conveniar.com.br>
2020-05-17 12:55:48 -0300

    estudos, props, projeto finalizado, componets

--- tecno/app.js
+++ tecno/app.js
@@ -1,72 +1,136 @@
 const vm = new Vue({
-    el: "#app",
-    data: {
-      produtos: [],
-      produto: false,
-      carrinho: [],
-    },
+  el: "#app",
+  data: {
+    produtos: [],
+    produto: false,
+    carrinho: [],
+    carrinhoAtivo: false,
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false,
+  },
 
-    filters: {
-      numeroPreco(valor) {
-        return valor.toLocaleString("pt-BR", {style: "currency", currency: "BRL"});
-      }
-    },
-    computed: {
-      carrinhoTotal(){
-        let total = 0
-        if(this.carrinho.length){
-          this.carrinho.forEach(item => {
-            total += item.preco;
-          });
-
-        }
-        return total;
-      }
-    },
-
-    methods: {
-      fecthProdutos(){
-        fetch("./api/produtos.json")
-          .then(r => r.json())
-          .then(r => {
-            this.produtos = r
-        })
-      },
-
-      fecthProduto(id){
-        fetch(`./api/produtos/${id}/dados.json`)
-          .then(r => r.json())
-          .then(r => {
-            this.produto = r;
-          })
-        },
-
-        fechar_modal({target, currentTarget}){
-            if(target === currentTarget) this.produto = false;
-        },
-
-        abrirModal(id){
-          this.fecthProduto(id);
-          window.scrollTo({
-            top:0,
-            behavior: "smooth"
-          })
-        },
-
-        adicionarItem(){
-          this.produto.estoque--;
-          const {id, nome, preco } = this.produto;
-          this.carrinho.push({id, nome, preco});
-        },
-
-        removerItem(index){
-          this.carrinho.splice(index, 1)
-        },
-    },
-
-    created() {
-      this.fecthProdutos();
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+      });
     }
+  },
+
+  computed: {
+    carrinhoTotal() {
+      let total = 0
+      if (this.carrinho.length) {
+        this.carrinho.forEach(item => {
+          total += item.preco;
+        });
+
+      }
+      return total;
+    }
+  },
+
+  methods: {
+    fecthProdutos() {
+      fetch("./api/produtos.json")
+        .then(r => r.json())
+        .then(r => {
+          this.produtos = r
+        })
+    },
+
+    fecthProduto(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then(r => r.json())
+        .then(r => {
+          this.produto = r;
+        })
+    },
+
+    fecharModal({target, currentTarget}) {
+      if (target === currentTarget) this.produto = false;
+    },
+
+    clickForaCarrinho({target, currentTarget}){
+        if (target === currentTarget) this.carrinhoAtivo = false;
+    },
+
+    abrirModal(id) {
+      this.fecthProduto(id);
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      })
+    },
+
+    adicionarItem() {
+      this.produto.estoque--;
+      const {
+        id,
+        nome,
+        preco
+      } = this.produto;
+      this.carrinho.push({
+        id,
+        nome,
+        preco
+      });
+      this.alerta(`${nome}, foi adicionado no carrinho.`);
+    },
+
+    removerItem(index) {
+      this.carrinho.splice(index, 1)
+    },
+
+    checarLocalStorage() {
+      if (window.localStorage.carrinho) {
+        this.carrinho = JSON.parse(window.localStorage.carrinho);
+      }
+    },
+
+    alerta(mensagem) {
+      this.mensagem = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1500);
+    },
+
+    compararEstoque(){
+      const items = this.carrinho.filter(({id}) => id === this.produto.id);
+      this.produto.estoque -= items.length;
+    },
+
+    router(){
+      const hash = document.location.hash;
+      if(hash){
+        this.fecthProduto(hash.replace('#', ''));
+      }
+    },
+  },
+
+  watch: {
+    produto(){
+      document.title = this.produto.nome || "Tecno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+      if(this.produto){
+        this.compararEstoque();
+      }
+
+    },
+
+    carrinho() {
+      window.localStorage.carrinho = JSON.stringify(this.carrinho);
+    }
+  },
+
+  created() {
+    this.fecthProdutos();
+    this.router();
+    this.checarLocalStorage();
+  }
 
 
 })
